a39744d27e41654fd09cc95253ddc8dce108af7d
James Reed <jamesreed.3050@gmail.com>
2023-03-15 13:08:40 +0900

    test-test

--- index.js
+++ index.js
@@ -4,11 +4,8 @@
 
 const app = require('./app');
 const config = require('./utils/config');
-const { info, error } = require('./utils/logger');
+const { info } = require('./utils/logger');
 
 app.listen(config.PORT, () => {
   info(`Server running on port ${config.PORT}`);
-})
-  .catch((e) => {
-    error('encountered error starting server', e.message);
-  });
+});
