a07f8ee3a75c98e839d9261f8e83ecb17722b1c0
James Reed <jamesreed.3050@gmail.com>
2023-03-16 20:30:56 +0900

    adding favourite likes helper and test

--- tests/list_helper.test.js
+++ tests/list_helper.test.js
@@ -1,8 +1,32 @@
 const listHelper = require('../utils/list_helper');
+const {blogs} = require('./testBlogs');
 
-test('dummy returns one', () => {
-  const blogs = [];
+describe('total likes', () => {
+  const listWithOneBlog = [
+    {
+      _id: '352342342351',
+      title: 'test blog to test testing',
+      author: 'Curtis E. Bear',
+      url: 'https://www.google.com',
+      likes: 7,
+      __v: 0,
+    },
+  ];
+  test('when list is one blog long, equals blog likes', () => {
+    const result = listHelper.totalLikes(listWithOneBlog);
+    expect(result).toBe(7);
+  });
+});
 
-  const result = listHelper.dummy(blogs);
-  expect(result).toBe(1);
+describe('favourite likes', () => {
+  test('returns one of the blogs with the most likes', () => {
+    const result = listHelper.favouriteLikes(blogs);
+    expect(result).toEqual(
+      {
+        title: 'Canonical string reduction',
+        author: 'Edsger W. Dijkstra',
+        likes: 12,
+      },
+    );
+  });
 });
--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,7 +1,18 @@
-const dummy = (blogs) => {
-  return 1;
+/* eslint-disable arrow-body-style */
+const totalLikes = (blogs) => blogs.reduce((sum, blog) => sum + blog.likes, 0);
+
+const favouriteLikes = (blogs) => {
+  const favorite = blogs.reduce((top, blog) => {
+    return blog.likes > top.likes ? blog : top;
+  });
+  return {
+    title: favorite.title,
+    author: favorite.author,
+    likes: favorite.likes,
+  };
 };
 
 module.exports = {
-  dummy
+  totalLikes,
+  favouriteLikes,
 };
